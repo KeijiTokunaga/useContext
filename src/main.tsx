6cb3a9c12ca542ddc425b87e4339c497ab62f24d
Keiji Tokunaga <keizi81@gmail.com>
2024-08-16 07:13:42 +0000

    不要なコンポーネントを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { StateProvider } from './context/StateProvider.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StateProvider>
-      <App />
-    </StateProvider>
+    <App />
   </StrictMode>,
 )

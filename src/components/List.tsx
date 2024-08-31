30814b73f7555443e04a977edee21a67b063a221
Keiji Tokunaga <keizi81@gmail.com>
2024-08-31 06:53:29 +0000

    react-windowを使う

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { faker } from '@faker-js/faker';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 
 interface Card {
     name: string;
@@ -7,35 +8,37 @@ interface Card {
     avatar: string;
 }
 
-const bigList: Card[] = [...Array(50000)].map(() => ({
-    name: faker.name.fullName(),
+const bigList: Card[] = Array.from({ length: 500000 }, () => ({
+    name: faker.person.fullName(),
     email: faker.internet.email(),
     avatar: faker.image.avatar(),
 }));
 
-const List = () => {
-    return (
-        <>
-            <div className="space-y-4 max-w-md mx-auto p-4">
-                {bigList.map((person, i) => {
-                    // avatarの中身をログに出力
-                    console.log(`Avatar URL for ${person.name}:`, person.avatar);
-
-                    return (
-                        <div key={i} className="bg-white border border-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-200">
-                            <div className="p-4 flex items-center">
-                                <img className="w-12 h-12 rounded-full mr-4" src={person.avatar} alt={`${person.name}'s avatar`} />
-                                <div>
-                                    <h2 className="font-semibold text-lg text-gray-800">{person.name}</h2>
-                                    <p className="text-gray-600">{person.email}</p>
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        </>
+const List: React.FC = () => {
+    const renderRow = ({ index, style }: ListChildComponentProps) => (
+        <div style={{ ...style, display: "flex" }}>
+            <img
+                src={bigList[index].avatar}
+                alt={bigList[index].name}
+                width={50}
+            />
+            <p>
+                {bigList[index].name} - {bigList[index].email}
+            </p>
+        </div>
     );
-}
+
+    return (
+        <FixedSizeList
+            height={window.innerHeight}
+            width={window.innerWidth - 20}
+            itemCount={bigList.length}q
+            
+            itemSize={50}
+        >
+            {renderRow}
+        </FixedSizeList>
+    );
+};
 
 export default List;
